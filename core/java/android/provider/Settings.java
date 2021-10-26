721f4f2faa2f4dd80b99273d068b04750ba16942
Beanstown <Beanstown@users.noreply.github.com>
2021-10-26 10:35:22 +0000

    base: incall vibration options [1/3]  *allow setting vibration when call is connected  *allow setting vibration when call is disconnected  *allow setting vibration for call waiting  *this works with google and aosp dialer :)
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I14a403189f37e45fa4b999ab015bde8f89ae3da4
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5572,6 +5572,24 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+         /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+         /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * These are all public system settings
          *
          * @hide
